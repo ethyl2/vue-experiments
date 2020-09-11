21a0bbd7dedd251c90c881eeaeccbaf303335c40
Heather Nuffer <zzzuzz@gmail.com>
2020-09-11 13:24:11 -0600

    Adds custom form validation

--- main.js
+++ main.js
@@ -11,6 +11,72 @@ Vue.component('product-details', {
     </ul>`,
 });
 
+Vue.component('product-review', {
+  template: `
+  <form class="review-form" @submit.prevent="onSubmit">
+
+  <p v-if="errors.length">
+    <b>Please correct the following error(s):</b>
+        <ul>
+            <li v-for="error in errors">{{ error }}</li>
+        </ul>
+  </p>
+  <p>
+    <label for="name">Name:</label>
+    <input id="name" v-model="name" placeholder="name">
+  </p>
+  
+  <p>
+    <label for="review">Review:</label>      
+    <textarea id="review" v-model="review" placeholder="what do you think?"></textarea>
+  </p>
+  
+  <p>
+    <label for="rating">Rating:</label>
+    <select id="rating" v-model.number="rating">
+      <option>5</option>
+      <option>4</option>
+      <option>3</option>
+      <option>2</option>
+      <option>1</option>
+    </select>
+  </p>
+      
+  <p>
+    <input type="submit" value="Submit">  
+  </p>    
+
+</form>
+    `,
+  data() {
+    return {
+      name: null,
+      review: null,
+      rating: null,
+      errors: [],
+    };
+  },
+  methods: {
+    onSubmit() {
+      if (this.name && this.review && this.rating) {
+        let productReview = {
+          name: this.name,
+          review: this.review,
+          rating: this.rating,
+        };
+        this.$emit('review-submitted', productReview);
+        this.name = null;
+        this.review = null;
+        this.rating = null;
+      } else {
+        if (!this.name) this.errors.push('Name required');
+        if (!this.review) this.errors.push('Review required');
+        if (!this.rating) this.errors.push('Rating required');
+      }
+    },
+  },
+});
+
 Vue.component('product', {
   props: {
     premium: {
@@ -77,6 +143,22 @@ Vue.component('product', {
         </button>
       </div>
     </div>
+
+    <product-review @review-submitted="addReview"></product-review>
+
+    <div>
+        <h2>Reviews</h2>
+        <p v-if="!reviews.length">No reviews yet.</p>
+        
+        <ul>
+            <li v-for="review in reviews">
+                <p>{{ review.name }}</p>
+                <p>{{ review.rating }}</p>
+                <p>{{ review.review}}</p>
+
+            </li>
+        </ul>
+    </div>
   </div>`,
   data() {
     return {
@@ -108,6 +190,7 @@ Vue.component('product', {
         },
       ],
       sizes: ['XS', 'S', 'M', 'L', 'XL'],
+      reviews: [],
     };
   },
   methods: {
@@ -122,6 +205,9 @@ Vue.component('product', {
         'remove-from-cart',
         this.variants[this.selectedVariant].variantId
       );
+    },
+    addReview(review) {
+      this.reviews.push(review);
     },
   },
   computed: {
