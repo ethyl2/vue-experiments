31d820c7c00d8abfbae44689cc22a5bc35ce500a
Heather Nuffer <zzzuzz@gmail.com>
2020-11-14 16:18:28 -0700

    Feat: Adds functionality to select a plan

--- vue_school/intro_to_components/planPicker/app.js
+++ vue_school/intro_to_components/planPicker/app.js
@@ -1,13 +1,61 @@
-Vue.component('plan-details', {
+const planDetails = {
     template: '#plan-template',
     props: {
         name: {
             type: String,
             default: 'The Basic'
+        },
+        selectedPlan : {
+            type: String
+        }
+    },
+    computed: {
+        isSelected() {
+            return this.name === this.selectedPlan
+        }
+    },
+    methods: {
+        select() {
+            this.$emit('select-plan', this.name)
+        }
+    }
+}
+
+// Example of registering a component locally
+
+const planPicker = {
+    template: "#plan-picker-template",
+    components: {
+        planDetails
+    },
+    data() {
+        return {
+            plans: ['The Curious', 'The Fruity Fan', 'The Addict', 'The Spicy Selector', 'The Cozy Cuddler'],
+            selectedPlan: null
+        }
+    },
+    methods: {
+        updatePlan(plan) {
+            this.selectedPlan = plan
+        }
+    }
+}
+
+// Example of a global component registration.
+
+Vue.component('app-intro', {
+    template: "#app-intro-template",
+    data() {
+        return {
+        appTitle: 'Herbal Tea Plans',
+        appSubtitle: 'We travel the the world to source the very best herbal teas for you'
         }
     }
 })
 
 new Vue({
-    el: '#app'
+    el: '#app',
+    components: {
+        planPicker
+    }
 })
