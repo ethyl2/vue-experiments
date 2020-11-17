0bdbca71457e631b66f420d3df0fce9bab588fe4
Heather Nuffer <zzzuzz@gmail.com>
2020-11-16 22:12:55 -0700

    Adds project for slots course

--- README.md
+++ README.md
@@ -6,4 +6,6 @@ This is a place for me to stash my experiments with [Vue.js](https://vuejs.org/)
 
 ## Courses I've completed include:
 
+[Vue School's Vue.js Fundamentals](https://vueschool.io/courses/vuejs-fundamentals)
+
 [Vue School's Vue.js Components Fundamentals](https://vueschool.io/courses/vuejs-components-fundamentals)
---
+++ vue_school/reusable_vuejs_components_with_slots/reusable-vuejs-components-with-slots-master/reusable-vuejs-components-with-slots-master/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="page">
+    <AppButton>Press Me</AppButton>
+    <AppButton>Like Me</AppButton>
+    <AppButton>Buy Me</AppButton>
+
+    <select v-model="selected">
+      <option 
+        v-for="option in options" 
+        :value="option.value" 
+        :key="option.value">
+        {{option.label}}
+      </option>
+    </select>
+
+
+    <AppUserList>
+      <template #userlist="{list}">
+        <AppUserCardsList :list="list">
+          <template #[selected]="{text}">
+            <h2>{{text}}</h2>
+          </template>
+        </AppUserCardsList>
+      </template>
+    </AppUserList>
+  </div>
+</template>
+
+<script>
+import AppUserList from "@/components/AppUserList";
+import AppUserCardsList from "@/components/AppUserCardsList";
+import AppButton from "@/components/AppButton"
+export default {
+  components: {
+    AppUserList,
+    AppUserCardsList,
+    AppButton
+  },
+  data() {
+    return {
+      selected: 'first',
+      options: [
+        { value: 'first',  label: 'first name'},
+        { value: 'last',  label: 'last name'},
+        { value: 'full',  label: 'fullname'},
+        { value: 'fullWithTitle',  label: 'fullname with title'},
+      ]
+    }
+  }
+};
+</script>
+
+<style>
+.page {
+  padding: 2rem;
+}
+
+.page > * + * {
+  margin-top: 2rem;
+}
+</style>
